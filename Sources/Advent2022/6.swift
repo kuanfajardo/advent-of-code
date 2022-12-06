c4e601875d79f3bc8fec280a1f6dd7b0bb52417c
Juan Diego Fajardo <jfajardo200@gmail.com>
2022-12-05 21:19:54 -0800

    2022 Day 6

---
+++ Sources/Advent2022/6.swift
@@ -0,0 +1,19 @@
+import AdventCommon
+import Algorithms
+import Regex
+
+/// https://adventofcode.com/2022/day/6
+public struct Day6: AdventDay {
+
+  public static let year = 2022
+  public static let day = 6
+
+  public static func solve(input: String) throws -> AdventAnswer {
+    let characters = Array(input)
+
+    return AdventAnswer(
+      partOne: characters.windows(ofCount: 4).firstIndex { Set($0).count == 4 },  // 1896
+      partTwo: characters.windows(ofCount: 14).firstIndex { Set($0).count == 14 }  // 3452
+    )
+  }
+}
