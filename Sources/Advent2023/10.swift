3a9f5103e622d29eec26d6a1d030e2cba2685f69
Juan Diego Fajardo <jfajardo200@gmail.com>
2023-12-22 16:26:34 -0800

    2023 Day 8 & 10

---
+++ Sources/Advent2023/10.swift
@@ -0,0 +1,220 @@
+//
+//  10.swift
+//
+//
+//  Created by Juan Fajardo on 12/18/23.
+//
+
+import AdventCommon
+import Algorithms
+
+public struct Day10: AdventDay {
+  
+  public static var year: Int { 2023 }
+  
+  public static var day: Int { 10 }
+  
+  enum Pipe: Character, Hashable {
+    case vertical = "|"
+    case horizontal = "-"
+    case northeast = "L"
+    case northwest = "J"
+    case southwest = "7"
+    case southeast = "F"
+    case ground = "."
+    case start = "S"
+  }
+  
+  typealias GridEntry = Grid<Pipe>.Entry
+  
+  enum Direction {
+    case north, east, south, west
+    
+    static prefix func ~(operand: Direction) -> Direction {
+      switch operand {
+      case .north: return .south
+      case .south: return .north
+      case .east: return .west
+      case .west: return .east
+      }
+    }
+  }
+  
+  public static func solve(input: String) throws -> AdventAnswer {
+    let rows = input.components(separatedBy: .newlines).map { $0.compactMap { Pipe(rawValue: $0) } }
+    var grid = Grid(rows: rows)
+    let start = grid.first { $0.value == .start }!
+
+    let (loop, startPipe) = self.findLoop(in: grid, startingAt: start)
+    grid[start.coordinate] = startPipe
+    
+    return AdventAnswer(
+      partOne: loop.count / 2,  // 6690
+      partTwo: self.numberOfTiles(insideLoop: loop, withinGrid: grid)  // 525
+    )
+  }
+  
+  // MARK: Part 1
+  
+  static func findLoop(in grid: Grid<Pipe>, startingAt start: GridEntry) -> (Set<Coordinate>, Pipe) {
+    let top = (
+      coordinate: grid.top(of: start.coordinate)!,
+      directionToPreviousCoordinate: Direction.south
+    )
+    let bottom = (
+      coordinate: grid.bottom(of: start.coordinate)!,
+      directionToPreviousCoordinate: Direction.north
+    )
+    let left = (
+      coordinate: grid.left(of: start.coordinate)!,
+      directionToPreviousCoordinate: Direction.west
+    )
+    let right = (
+      coordinate: grid.right(of: start.coordinate)!, 
+      directionToPreviousCoordinate: Direction.east
+    )
+      
+    let _loop: [Coordinate] = [top, bottom, left, right].lazy.firstNonNil {
+      try? visit(grid: grid, start: start.coordinate, firstCoordinate: $0.coordinate, comingFrom: $0.directionToPreviousCoordinate)
+    }!
+    let loop = Set(_loop)
+    
+    let startNeighbors: Set<Coordinate> = [
+      _loop[1],
+      _loop[_loop.count - 2]  // Start is at both beginning and end.
+    ]
+
+    let startPipe: Pipe
+    if startNeighbors == [top.coordinate, bottom.coordinate] {
+      startPipe = .vertical
+    } else if startNeighbors == [top.coordinate, left.coordinate] {
+      startPipe = .northwest
+    } else if startNeighbors == [top.coordinate, right.coordinate] {
+      startPipe = .northeast
+    } else if startNeighbors == [left.coordinate, right.coordinate] {
+      startPipe = .horizontal
+    } else if startNeighbors == [left.coordinate, bottom.coordinate] {
+      startPipe = .southwest
+    } else if startNeighbors == [right.coordinate, bottom.coordinate] {
+      startPipe = .southeast
+    } else {
+      fatalError("Unknown start pipe.")
+    }
+        
+    return (loop, startPipe)
+  }
+  
+  enum LoopError: Error {
+    case alreadyVisited
+    case badCoordinate
+    case invariantViolated
+  }
+  
+  static func visit(
+    grid: Grid<Pipe>,
+    start: Coordinate,
+    firstCoordinate: Coordinate,
+    comingFrom directionToPreviousCoordinate: Direction
+  ) throws -> [Coordinate] {
+    var loop = [start]
+    var coordinate = firstCoordinate
+    var directionToPreviousCoordinate = directionToPreviousCoordinate
+    var visited: Set<Coordinate> = [start]
+
+    while true {
+      loop.append(coordinate)
+
+      if coordinate == start {
+        return loop
+      }
+
+      if visited.contains(coordinate) {
+        throw LoopError.alreadyVisited
+      }
+
+      visited.insert(coordinate)
+      
+      let directionToNextCoordinate: Direction
+      let value: Pipe = grid[coordinate]
+      switch (value, directionToPreviousCoordinate) {
+      case (.horizontal, .west), (.northeast, .north), (.southeast, .south):
+        directionToNextCoordinate = .east
+      case (.horizontal, .east), (.northwest, .north), (.southwest, .south):
+        directionToNextCoordinate = .west
+      case (.vertical, .north), (.southeast, .east), (.southwest, .west):
+        directionToNextCoordinate = .south
+      case (.vertical, .south), (.northeast, .east), (.northwest, .west):
+        directionToNextCoordinate = .north
+      default:
+        throw LoopError.invariantViolated
+      }
+      
+      let nextCoordinate: Coordinate?
+      switch directionToNextCoordinate {
+      case .north: nextCoordinate = grid.top(of: coordinate)
+      case .south: nextCoordinate = grid.bottom(of: coordinate)
+      case .west: nextCoordinate = grid.left(of: coordinate)
+      case .east: nextCoordinate = grid.right(of: coordinate)
+      }
+      
+      guard let nextCoordinate else { throw LoopError.badCoordinate }
+      
+      coordinate = nextCoordinate
+      directionToPreviousCoordinate = ~directionToNextCoordinate
+    }
+  }
+  
+  // MARK: Part 2
+  
+  indirect enum RayTracingState {
+    case outside
+    case inside
+    case pending(current: RayTracingState, expecting: Pipe)
+    
+    mutating func flip() {
+      switch self {
+      case .outside: self = .inside
+      case .inside: self = .outside
+      case .pending(let current, _):
+        self = current
+        self.flip()
+      }
+    }
+  }
+  
+  // https://en.wikipedia.org/wiki/Point_in_polygon#Ray_casting_algorithm
+  static func numberOfTiles(insideLoop loop: Set<Coordinate>, withinGrid grid: Grid<Pipe>) -> Int {
+    var tilesInside = 0
+    for row in grid.rows {
+      var state: RayTracingState = .outside
+      for entry in row {
+        if loop.contains(entry.coordinate) {
+          switch entry.value {
+          case .horizontal: break
+          case .vertical: state.flip()
+          case .northeast: state = .pending(current: state, expecting: .southwest)
+          case .southeast: state = .pending(current: state, expecting: .northwest)
+          case .northwest, .southwest:
+            guard case .pending(let current, let expecting) = state else {
+              fatalError("Not possible.")
+            }
+            if entry.value == expecting {
+              state.flip()
+            } else {
+              state = current
+            }
+
+          case .ground, .start:
+            fatalError("Not possible.")
+          }
+        } else {
+          if case .inside = state {
+            tilesInside += 1
+          }
+        }
+      }
+    }
+    
+    return tilesInside
+  }
+}
---
+++ Sources/Advent2023/8.swift
@@ -0,0 +1,214 @@
+//
+//  8.swift
+//
+//
+//  Created by Juan Fajardo on 12/16/23.
+//
+
+import AdventCommon
+import RegexBuilder
+import Algorithms
+
+fileprivate typealias Regex = _StringProcessing.Regex
+
+public struct Day8: AdventDay {
+  
+  public static var year: Int { 2023 }
+  
+  public static var day: Int { 8 }
+  
+  public static func solve(input: String) throws -> AdventAnswer {
+    let temp =
+    """
+    RL
+
+    AAA = (BBB, CCC)
+    BBB = (DDD, EEE)
+    CCC = (ZZZ, GGG)
+    DDD = (DDD, DDD)
+    EEE = (EEE, EEE)
+    GGG = (GGG, GGG)
+    ZZZ = (ZZZ, ZZZ)
+    """
+    
+    let temp2 =
+    """
+    LLR
+
+    AAA = (BBB, BBB)
+    BBB = (AAA, ZZZ)
+    ZZZ = (ZZZ, ZZZ)
+    """
+    
+    var lines = input.components(separatedBy: .newlines)
+    let instructions = lines.removeFirst()
+    let nodes = try lines.filter { !$0.isEmpty }.map(parseNode)
+    print(nodes.count)
+    
+    let leftNodesMap = [String: String](uniqueKeysWithValues: nodes.map { (key: $0.id, value: $0.left) })
+    let rightNodesMap = [String: String](uniqueKeysWithValues: nodes.map { (key: $0.id, value: $0.right) })
+    
+    let partOne = self.numStepsTillEnd(
+      instructions: instructions,
+      initial: "AAA",
+      next: { current, instruction in
+        switch instruction {
+        case "L": return leftNodesMap[current]!
+        case "R": return rightNodesMap[current]!
+        default: fatalError("Invalid instruction: \(instruction)!")
+        }
+      },
+      isDone: {
+        $0 == "ZZZ"
+      }
+    )
+    
+    // Cycle in each one
+    // Zs can occur before cycle or after
+    // Find formula for Zs for each cycle
+    // Z_0 = [1, 4, 5, ...] UNION [(size of cycle)*N + X for X in Zs in cycle]
+    
+    //  170, but starting on 3rd
+    //  986 + 75n
+    //
+    
+//    let partTwo = self.numStepsTillEnd(
+//      instructions: instructions,
+//      initial: nodes.filter { $0.id.last == "A" }.map(\.id),
+//      next: { current, instruction in
+//        switch instruction {
+//        case "L": return current.map { leftNodesMap[$0]! }
+//        case "R": return current.map { rightNodesMap[$0]! }
+//        default: fatalError("Invalid instruction: \(instruction)!")
+//        }
+//      },
+//      isDone: {
+//        $0.allSatisfy { $0.last == "Z" }
+//      }
+//    )
+    
+    return AdventAnswer(
+      partOne: partOne,  // 21883
+      partTwo: nodes.filter { $0.id.last == "A" }.map(\.id).map { self.computeCycle(start: $0, lefts: leftNodesMap, rights: rightNodesMap, instructions: instructions)}  //
+    )
+  }
+  
+  static func numStepsTillEnd<T>(
+    instructions: String,
+    initial: T,
+    next: (T, Character) -> T,
+    isDone: (T) -> Bool
+  ) -> Int {
+    var numSteps = 0
+    var current = initial
+    for instruction in instructions.cycled() {
+      if isDone(current) {
+        break
+      }
+      numSteps += 1
+      current = next(current, instruction)
+    }
+    
+    return numSteps
+  }
+  
+  static func computeCycle(
+    start: String,
+    lefts: [String: String],
+    rights: [String: String],
+    instructions: String
+  ) -> (Int, Int, Int, String, [(Int, String)]) {
+    var numSteps = 0
+    var current = start
+    var visited = [Int: [String]].init(uniqueKeysWithValues: (0..<instructions.count).map { ($0, []) })
+    var loopStart: Int?
+    var nodes = [current]
+    for (i, instruction) in Array(instructions.enumerated()).cycled() {
+      if visited[i]!.contains(current) {
+        loopStart = i
+        break
+      }
+      visited[i]!.append(current)
+      nodes.append(current)
+      numSteps += 1
+      switch instruction {
+      case "L": current = lefts[current]!
+      case "R": current = rights[current]!
+      default: fatalError("Invalid instruction: \(instruction)!")
+      }
+    }
+    let loopLength = (numSteps - loopStart!) / instructions.count
+    let visitedLoopStart = visited[loopStart!]!.enumerated()
+    let loopNStart = visitedLoopStart.first {
+      $0.element == current
+    }!.offset
+    let numStepLoopStart = loopNStart * instructions.count + loopStart!
+    return (numSteps, loopStart!, loopLength, current, Array(visitedLoopStart))
+  }
+  
+//  class Node {
+//    let id: String
+//    let left: Node
+//    let right: Node
+//    
+//    private init(id: String, left: Node, right: Node) {
+//      self.id = id
+//      self.left = left
+//      self.right = right
+//    }
+//    
+//    static var nodes = [String: Node]()
+//    
+//    static func make(id: String, left: Node, right: Node) -> Node {
+//      guard self.nodes[id] == nil else {
+//        fatalError("More than one node initialized with id: \(id)!")
+//      }
+//      let node = Node(id: id, left: left, right: right)
+//      nodes[id] = node
+//      return node
+//    }
+//    
+//    static func node(id: String) -> Node? {
+//      self.nodes[id]
+//    }
+//  }
+  
+  struct Node {
+    let id: String
+    let left: String
+    let right: String
+  }
+  
+  private static func parseNode(_ raw: String) throws -> Node {
+    func nodeIDCapture(as reference: RegexBuilder.Reference<String>) -> some RegexComponent {
+      return Regex {
+        TryCapture(as: reference) {
+          Repeat(count: 3) {
+            CharacterClass.word
+          }
+        } transform: {
+          String($0)
+        }
+      }
+    }
+    
+    let idRef = Reference(String.self)
+    let leftRef = Reference(String.self)
+    let rightRef = Reference(String.self)
+    
+    let regex = Regex {
+      nodeIDCapture(as: idRef)
+      " = ("
+      nodeIDCapture(as: leftRef)
+      ", "
+      nodeIDCapture(as: rightRef)
+      ")"
+    }
+    
+    guard let match = try regex.wholeMatch(in: raw) else {
+      throw AdventError.malformedInput(input: raw)
+    }
+    
+    return Node(id: match[idRef], left: match[leftRef], right: match[rightRef])
+  }
+}
